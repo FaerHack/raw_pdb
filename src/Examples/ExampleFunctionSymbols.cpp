2e7caef2356d3f41b71320bb1874f4ccfc206ae9
FaerHack <makcneyctroev@gmail.com>
2022-10-03 21:18:49 +0300

    Fixed function symbol size calculation

--- src/Examples/ExampleFunctionSymbols.cpp
+++ src/Examples/ExampleFunctionSymbols.cpp
@@ -210,8 +210,7 @@ void ExampleFunctionSymbols(const PDB::RawFile& rawPdbFile, const PDB::DBIStream
 			}
 
 			const FunctionSymbol& nextSymbol = functionSymbols[i + 1u];
-			const size_t size = nextSymbol.rva - currentSymbol.rva;
-			(void)size; // unused
+			currentSymbol.size = nextSymbol.rva - currentSymbol.rva;
 			++foundCount;
 		}
 
